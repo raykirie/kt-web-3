8a6182dd41174eb77458cb2bc5a283636e684281
raykirie <145163524+raykirie@users.noreply.github.com>
2023-11-30 16:20:58 +0300

    Update card-component.js

--- card-component.js
+++ card-component.js
@@ -1,30 +1,30 @@
-// Создаем пользовательский веб-компонент
+
 class CardComponent extends HTMLElement {
     constructor() {
         super();
 
-        // Создаем Shadow DOM для компонента
+        
         this.attachShadow({ mode: 'open' });
 
-        // Создаем основную структуру карточки
+       
         const card = document.createElement('div');
         card.classList.add('card');
 
-        // Получаем шаблоны для заголовка и контента
+        
         const headerTemplate = this.querySelector('template[slot="header"]');
         const contentTemplate = this.querySelector('template[slot="content"]');
 
-        // Клонируем содержимое шаблонов и добавляем их к карточке
+        
         const headerClone = document.importNode(headerTemplate.content, true);
         const contentClone = document.importNode(contentTemplate.content, true);
 
         card.appendChild(headerClone);
         card.appendChild(contentClone);
 
-        // Вставляем карточку в Shadow DOM
+        
         this.shadowRoot.appendChild(card);
 
-        // Применяем стили
+        
         const style = document.createElement('style');
         style.textContent = `
             .card {
